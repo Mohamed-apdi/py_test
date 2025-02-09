c25158f884b41d790957dec72f6f16532e202efe
fardowsa <fardowsahersi31@gmail.com>
2025-02-09 09:33:57 +0300

    add fixed

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ def add(addend1, addend2):
     return addend1 + addend2
 print(add(1, 2))
 
-#   git config --global user.email "you@example.com"
-#   git config --global user.name "Your Name
+# git add .  add all change
+# git commit -m "add main.py"  commit change
+# git push origin dowsa  push change to github
+
+
