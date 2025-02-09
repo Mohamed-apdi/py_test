0ffeee4307fe359a3c5f22edc6aa6709faf5914f
fardowsa <fardowsahersi31@gmail.com>
2025-02-09 09:26:44 +0300

    added sum function

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("hello world")
+
+def add(addend1, addend2):
+    return addend1 + addend2
+print(add(1, 2))
+
+#   git config --global user.email "you@example.com"
+#   git config --global user.name "Your Name
